115c547655588ed18a4ba3371f9414c64987292a
Prijam Khanal <khanalpriju65@gmail.com>
2024-12-27 20:09:48 -0600

    Responsive Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import styles from "./styles/LandingFrame.module.css";
-import { Header } from "./components/sections/Header";
+
 import { Introduction } from "./components/sections/Introduction";
 import { Features } from "./components/sections/Feature";
 import { ClassFeature } from "./components/sections/ClassFeature";
 import { LaunchSection } from "./components/sections/LaunchSection";
 import { UpcomingFeatures } from "./components/sections/UpcomingFeature";
 import { Footer } from "./components/sections/Footer";
+import { LandingPageHeader } from "./components/header/Header";
 
 function App() {
   return (
     <div className={styles.landingFrame}>
-      <Header />
+      <LandingPageHeader />
       <div className={styles.slide7Parent}>
         <Introduction />
         <Features />
