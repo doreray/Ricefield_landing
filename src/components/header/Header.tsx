ff820cdbd66077652d96615f63b5c539cbcfa5fd
Prijam Khanal <khanalpriju65@gmail.com>
2025-01-18 16:20:12 -0500

    Merged, Solved all the conflict

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,29 +1,8 @@
-import React, { useState } from "react";
-import { Menu } from "lucide-react";
 import { useState } from "react";
 import { Menu } from "lucide-react";
 import styles from "./Header.module.css";
 
 export function LandingPageHeader() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const handleJoinWaitlistClick = () => {
-    window.open("https://forms.gle/XgjRixUwhZ3ZkLe9", "_blank");
-  };
-
-  const handleFarmersDictClick = () => {
-    window.open(
-      "https://joinricefield.notion.site/Farmers-Dictionary-1043b99ded3180a68889ec80aa0bace7",
-      "_blank"
-    );
-  };
-
-  const handlePrototypeClick = () => {
-    window.open(
-      "https://www.figma.com/proto/XSI4ZVnYeubv5XS0fZuwvB/ricefield---editor?node-id=4225-18146&node-type=frame&t=MvhnGZxlXF5xTW4g-1&scaling=scale-down-width&content-scaling=fixed&page-id=4037%3A16555&starting-point-node-id=4248%3A15497",
-      "_blank"
-    );
-  };
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleJoinWaitlistClick = () => {
@@ -195,4 +174,3 @@ export function LandingPageHeader() {
     </div>
   );
 }
-
